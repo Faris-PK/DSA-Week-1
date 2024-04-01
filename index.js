87d0d31696cfe2fd73a46f6a1f258e5d5a6d0692
Faris Pk <faripk369@gmail.com>
2024-04-01 12:30:41 +0530

    array question done

--- index.js
+++ index.js
@@ -28,3 +28,63 @@
  
 //  sum = n*(n+1)/2;
 //  console.log('sum:',sum);
+
+
+                  //Arary Problem : 1 
+
+// function twoNumberSum(array, target) {
+//     for (let i = 0; i < array.length - 1; i++) {
+//         for (let j = i + 1; j < array.length; j++) {
+//             if (array[i] + array[j] === target) {
+//                 return [array[i], array[j]];
+//             }
+//         }
+//     }
+//     return [];
+// }
+
+// // Example usage:
+// const inputArray = [3, 5, 2, -4, 8];
+// const targetSum = 7;
+
+// const result = twoNumberSum(inputArray, targetSum);
+// console.log(result);
+
+                    //array Problem : 2
+
+//   function shuffle(nums, n) {
+
+//     let x =[]
+//     for(i=0;i<n;i++){
+//         x.push(nums[i],nums[i+n])
+//     }
+//     return x;
+
+    
+// };        
+
+// let nums = [2,5,1,3,4,7]
+// let n = 3;
+// let result = shuffle(nums,n)
+
+// console.log(result);
+
+                     //array Problem : 3
+
+var targetIndices = function(nums, target) {
+let arr = []
+let sortedNums = [...nums].sort((a, b) => a - b);
+for(i=0;i<=sortedNums.length;i++){
+        if(sortedNums[i]==target){
+            arr.push(i)
+        }
+}
+
+return arr
+};  
+
+let nums =  [1,2,5,2,3]
+let n = 2;
+let result = targetIndices(nums,n)
+
+console.log(result);
