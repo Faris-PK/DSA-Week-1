5d3d2f80914e67fed3cc851ae969cc136f84c325
Faris Pk <faripk369@gmail.com>
2024-04-01 10:01:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// let start = performance.now();
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+// let end = performance.now();
+
+// console.log('start:', start);
+// console.log('end:', end);
+// console.log(end-start);
+
+
+                //Linear Time complexity
+
+// let n = 4
+// let sum = 0;
+// for(let i=0;i<=n;i++){
+//      sum=sum+i;
+// }
+// console.log('sum:',sum);
+ 
+
+                //Constant Time Complexity
+
+//  let n = 4
+//  let sum = 0; 
+ 
+//  sum = n*(n+1)/2;
+//  console.log('sum:',sum);
